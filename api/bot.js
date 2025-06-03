b14888023fb84ff9b0b82d14b57daca4260e066a
Zhang Yuan <zhang_yuan@franknetwork.co>
2025-06-03 13:52:44 +0800

    Cleanup

--- api/bot.js
+++
@@ -1,7 +0,0 @@
-import TelegramBot from 'node-telegram-bot-api';
-
-const token = process.env.TELEGRAM_BOT_TOKEN;
-const chatId = process.env.TELEGRAM_CHAT_ID;
-const bot = new TelegramBot(token, { polling: true });
-
-export default bot;
--- api/sendPoll.js
+++ api/sendPoll.js
@@ -1,4 +1,8 @@
-import bot from "./bot";
+import TelegramBot from 'node-telegram-bot-api';
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const chatId = process.env.TELEGRAM_CHAT_ID;
+const bot = new TelegramBot(token, { polling: true });
 
 export default async function handler(req, res) {
     const question = "No meals for me on";
@@ -34,35 +38,3 @@ export default async function handler(req, res) {
         res.status(500).send("Failed to send poll.");
     }   
 }
-
-bot.onText(/\/sendpoll/, async (msg) => {
-    const chatId = msg.chat.id;
-    const question = "No meals for me on";
-    const weekdays = [
-        "Monday Lunch", "Monday Dinner",
-        "Tuesday Lunch", "Tuesday Dinner",
-        "Wednesday Lunch", "Wednesday Dinner",
-        "Thursday Lunch", "Thursday Dinner",
-        "Friday Lunch", "Friday Dinner",
-    ];
-    const weekends = [
-        "Saturday Lunch", "Saturday Dinner",
-        "Sunday Lunch", "Sunday Dinner"
-    ];
-
-    try {
-        await Promise.all([
-            bot.sendPoll(chatId, question, weekdays, {
-                is_anonymous: false,
-                allows_multiple_answers: true
-            }),
-            bot.sendPoll(chatId, question, weekends, {
-                is_anonymous: false,
-                allows_multiple_answers: true
-            })
-        ]);
-    } catch (error) {
-        console.error(error);
-        bot.sendMessage(chatId, "Failed to send poll.");
-    }
-});
