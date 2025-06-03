76ebc1b54956d6d87936115cba17b11e12aa10a8
Zhang Yuan <zhang_yuan@franknetwork.co>
2025-06-03 13:23:06 +0800

    refactor code

--- api/bot.js
+++ api/bot.js
@@ -1,10 +1,7 @@
 import TelegramBot from 'node-telegram-bot-api';
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
-const bot = new TelegramBot(token, { polling: true }); // Enable polling to receive messages
+const chatId = process.env.TELEGRAM_CHAT_ID;
 
-bot.on('message', (msg) => {
-  console.log("Chat ID:", msg.chat.id);
-  console.log("Chat Type:", msg.chat.type);
-  console.log("Chat Title:", msg.chat.title);
-});
+const bot = new TelegramBot(token, { polling: false });
+
