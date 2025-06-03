273f9facf2492250e9d8f757d62aee78c1cbe51f
Zhang Yuan <zhang_yuan@franknetwork.co>
2025-06-03 13:24:26 +0800

    Allow bot to read messages

--- api/bot.js
+++ api/bot.js
@@ -2,6 +2,6 @@ import TelegramBot from 'node-telegram-bot-api';
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const chatId = process.env.TELEGRAM_CHAT_ID;
+const bot = new TelegramBot(token, { polling: true });
 
-const bot = new TelegramBot(token, { polling: false });
-
+export default bot;
